7c87148ca05444862145e6c46a232e8d4a73b22a
shiro <kouyabepafing@gmail.com>
2025-12-26 10:45:08 +0100

    Ajout du projet cal.html

---
+++ poly.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+int degre;
+float* table;
+ 
+ printf("\tBienvenue!\nCeci est un programme qui vous permet d'entrer le degre de votre polynome P et calculer P(a)\n\n Choisissez le degre de votre polynome: \n");
+ scanf("%d", degre);
+ for(int i=0; i<=degre; i++){
+ scanf("%f", table[i]);
+ float exposant(float x, int n){
+ float resultat=exposant(x,n)
+ resultat=x^n;
+ }
+ }
+
+
+return 0;
+
+}
